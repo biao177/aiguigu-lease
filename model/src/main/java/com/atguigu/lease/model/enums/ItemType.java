e76e92aa6bf2687289c40785d92de8463d5b1cc7
biao <biao177@qq.com>
2025-11-13 04:49:35 +0800

    说明@EnumValue和@JsonValue注解的作用

--- model/src/main/java/com/atguigu/lease/model/enums/ItemType.java
+++ model/src/main/java/com/atguigu/lease/model/enums/ItemType.java
@@ -13,6 +13,33 @@ public enum ItemType implements BaseEnum {
 
     @EnumValue
     @JsonValue
+    /*
+    * @EnumValue:mybatis-plus提供,将枚举对象和属性进行映射
+    * ps:当前标记在 code 字段上面。
+    *
+    * 以ItemType.APARTMENT对象为例，查询数据库的时候，会将ItemType.APARTMENT.code的值传入到sql语句中
+    *
+    *   没加@EnumValue注解的时候
+    *       sql=>SELECT id,type,name,create_time,update_time,is_deleted FROM label_info WHERE is_deleted=0 AND (type = APARTMENT)
+    *       type = APARTMENT，为当前枚举ItemType.APARTMENT的名称
+    *
+    *   添加@EnumValue注解的时候
+    *       sql=>SELECT id,type,name,create_time,update_time,is_deleted FROM label_info WHERE is_deleted=0 AND (type = 1)
+    *       type = 1，为当前枚举ItemType.APARTMENT.code的值
+    * */
+
+    /*
+     * @JsonValue:jackson提供,将枚举对象和属性进行映射
+     * ps:当前标记在 code 字段上面。
+     *
+     * 以ItemType.APARTMENT对象为例，查询数据库的时候，mybatis-plus已将查询到的记录封装成了ItemType.APARTMENT对象
+     *
+     *   没加@JsonValue
+     *       返回给前端的数据=>"type": "APARTMENT",
+     *
+     *   添加@JsonValue
+     *       返回给前端的数据=>"type": '1',
+     * */
     private Integer code;
     private String name;
 
