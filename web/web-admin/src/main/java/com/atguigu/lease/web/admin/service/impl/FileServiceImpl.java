d98a46b11a3549e0f16f6b3f5b0c5c6040d64733
biao <biao177@qq.com>
2025-11-14 03:03:17 +0800

    109p.实现minioClient上传文件的功能

--- web/web-admin/src/main/java/com/atguigu/lease/web/admin/service/impl/FileServiceImpl.java
+++ web/web-admin/src/main/java/com/atguigu/lease/web/admin/service/impl/FileServiceImpl.java
@@ -1,9 +1,69 @@
 package com.atguigu.lease.web.admin.service.impl;
 
+import com.atguigu.lease.common.minio.MinioProperties;
 import com.atguigu.lease.web.admin.service.FileService;
+import io.minio.*;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.UUID;
 
 @Service
 public class FileServiceImpl implements FileService {
+    @Autowired
+    private MinioClient minioClient;
+    @Autowired
+    private MinioProperties minioProperties;
 
+    @Override
+    public String upload(MultipartFile file) {
+        // 判断minio中是否存在桶
+        try {
+            boolean bucketExists = minioClient.bucketExists(BucketExistsArgs.builder().bucket(minioProperties.getBucketName()).build());
+            if (!bucketExists) {
+                // 创建桶
+                minioClient.makeBucket(MakeBucketArgs.builder()
+                        .bucket(minioProperties.getBucketName())
+                        .build());
+                // 设置策略
+                minioClient.setBucketPolicy(SetBucketPolicyArgs.builder()
+                        .bucket(minioProperties.getBucketName())
+                        .config(createBucketPolicyConfig(minioProperties.getBucketName()))
+                        .build());
+            }
+            // 上传文件
+            String filename = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyyMMdd")) +
+                    "/" + UUID.randomUUID() + "-" + file.getOriginalFilename();
+            minioClient.putObject(
+                    PutObjectArgs.builder()
+                            .bucket(minioProperties.getBucketName())
+                            .stream(file.getInputStream(), file.getSize(), -1)
+                            .object(filename)
+                            .contentType(file.getContentType())//设置文件的类型，让浏览器解析的时候是浏览文件，不是下载文件
+                            .build());
+
+            return String.join("/", minioProperties.getEndpoint(), minioProperties.getBucketName(), filename);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    private String createBucketPolicyConfig(String bucketName) {
+
+        return """
+                {
+                  "Statement" : [ {
+                    "Action" : "s3:GetObject",
+                    "Effect" : "Allow",
+                    "Principal" : "*",
+                    "Resource" : "arn:aws:s3:::%s/*"
+                  } ],
+                  "Version" : "2012-10-17"
+                }
+                """.formatted(bucketName);
+    }
 }
