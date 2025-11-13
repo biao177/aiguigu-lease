2ad5a95483766a446d5def5bdb87b4dba9123d49
biao <biao177@qq.com>
2025-11-13 17:01:23 +0800

    105p.实现房间杂费管理的新增、查询、删除逻辑

--- web/web-admin/src/main/java/com/atguigu/lease/web/admin/service/impl/FeeKeyServiceImpl.java
+++ web/web-admin/src/main/java/com/atguigu/lease/web/admin/service/impl/FeeKeyServiceImpl.java
@@ -3,8 +3,11 @@ package com.atguigu.lease.web.admin.service.impl;
 import com.atguigu.lease.model.entity.FeeKey;
 import com.atguigu.lease.web.admin.mapper.FeeKeyMapper;
 import com.atguigu.lease.web.admin.service.FeeKeyService;
+import com.atguigu.lease.web.admin.vo.fee.FeeKeyVo;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 /**
 * @author liubo
@@ -15,6 +18,10 @@ import org.springframework.stereotype.Service;
 public class FeeKeyServiceImpl extends ServiceImpl<FeeKeyMapper, FeeKey>
     implements FeeKeyService{
 
+    @Override
+    public List<FeeKeyVo> listFeeInfo() {
+        return baseMapper.listFeeInfo();
+    }
 }
 
 
