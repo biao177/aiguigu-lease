ea31b204f5f59117eff162d877dca481fe4d706c
biao <biao177@qq.com>
2025-11-14 11:16:37 +0800

    147p.实现后台登录系统系统查询登录用户信息

--- web/web-admin/src/main/java/com/atguigu/lease/web/admin/custom/interceptor/AuthenticationInterceptor.java
+++ web/web-admin/src/main/java/com/atguigu/lease/web/admin/custom/interceptor/AuthenticationInterceptor.java
@@ -1,6 +1,9 @@
 package com.atguigu.lease.web.admin.custom.interceptor;
 
+import com.atguigu.lease.common.login.LoginUser;
+import com.atguigu.lease.common.login.LoginUserHolder;
 import com.atguigu.lease.common.utils.JwtUtil;
+import io.jsonwebtoken.Claims;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Component;
@@ -12,7 +15,19 @@ public class AuthenticationInterceptor implements HandlerInterceptor {
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         String token = request.getHeader("access-token");
-        JwtUtil.parseToken(token);
+
+        Claims claims = JwtUtil.parseToken(token);
+        Long userId = claims.get("userId", Long.class);
+        String username = claims.get("username", String.class);
+        // 设置 ThreadLocal
+        LoginUserHolder.setLoginUser(new LoginUser(userId, username));
+
         return true;
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+        // remove ThreadLocal
+        LoginUserHolder.clear();
     }
 }
