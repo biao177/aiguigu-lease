796b18243d4d820e9276089f74e179a0ef5a12a3
biao <biao177@qq.com>
2025-11-15 03:48:13 +0800

    159p.1、实现app端个人中心-保存浏览历史 2、启用异步注解@Async

--- web/web-app/src/main/java/com/atguigu/lease/web/app/service/impl/BrowsingHistoryServiceImpl.java
+++ web/web-app/src/main/java/com/atguigu/lease/web/app/service/impl/BrowsingHistoryServiceImpl.java
@@ -8,6 +8,7 @@ import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
@@ -23,5 +24,25 @@ public class BrowsingHistoryServiceImpl extends ServiceImpl<BrowsingHistoryMappe
     @Override
     public IPage<HistoryItemVo> pageHistoryItemByUserId(Page<HistoryItemVo> page, Long userId) {
         return baseMapper.pageHistoryItemByUserId(page, userId);
+    }
+
+    @Override
+    @Async
+    public void saveHistory(Long userId, Long id) {
+        LambdaQueryWrapper<BrowsingHistory> queryWrapper = new LambdaQueryWrapper<>();
+        queryWrapper.eq(BrowsingHistory::getUserId, userId);
+        queryWrapper.eq(BrowsingHistory::getRoomId, id);
+        BrowsingHistory browsingHistory = baseMapper.selectOne(queryWrapper);
+
+        if (browsingHistory != null) {
+            browsingHistory.setBrowseTime(LocalDateTime.now());
+            baseMapper.updateById(browsingHistory);
+        } else {
+            BrowsingHistory newBrowsingHistory = new BrowsingHistory();
+            newBrowsingHistory.setUserId(userId);
+            newBrowsingHistory.setRoomId(id);
+            newBrowsingHistory.setBrowseTime(LocalDateTime.now());
+            baseMapper.insert(newBrowsingHistory);
+        }
     }
 }
