f10e9e5dd6b6f4453e86f24aeecdcb3d46b64e64
biao <biao177@qq.com>
2025-11-14 10:33:52 +0800

    145p.实现后台系统登录接口

---
+++ common/src/main/java/com/atguigu/lease/common/utils/JwtUtil.java
@@ -0,0 +1,25 @@
+package com.atguigu.lease.common.utils;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+
+import javax.crypto.SecretKey;
+import java.util.Date;
+
+public class JwtUtil {
+
+    private static final long tokenExpiration = 60 * 60 * 1000L;
+    private static final SecretKey tokenSignKey = Keys.hmacShaKeyFor("M0PKKI6pYGVWWfDZw90a0lTpGYX1d4AQ".getBytes());
+
+    public static String createToken(Long userId, String username) {
+        String token = Jwts.builder().
+                setSubject("USER_INFO").
+                setExpiration(new Date(System.currentTimeMillis() + tokenExpiration)).
+                claim("userId", userId).
+                claim("username", username).
+                signWith(tokenSignKey, SignatureAlgorithm.HS256).
+                compact();
+        return token;
+    }
+}
